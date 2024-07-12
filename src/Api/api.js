0b85cc6e894d26a7601c313f7d18f3472f3b15ed
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-07-12 22:10:55 +0530

    chnaged url

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 // const LOCALHOST='http://localhost:8080'
-export const API_BASE_URL="https://proassign-be-production.up.railway.app"
+export const API_BASE_URL="https://captivating-courage-production.up.railway.app/"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
