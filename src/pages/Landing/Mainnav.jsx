c16cfc34ea73b5ccc520079860f2c82d5639e1c2
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-09-27 19:13:30 +0530

    fixed navbar border b

--- src/pages/Landing/Mainnav.jsx
+++ src/pages/Landing/Mainnav.jsx
@@ -7,8 +7,8 @@ const Mainnav = () => {
 
   return (
     <div>
-        <div className='fixed top-0 left-0 z-40 h-[73px] w-full bg-white flex items-center border-b border-gray-200 shadow-md'>
-             <div className='bg-blue-600 h-full w-[74px] items-start mr-16'><img src={applogo} className='items-center justify-between size-[70px] object-contain'/></div>
+        <div className='fixed top-0 left-0 z-40 h-[74px] w-full bg-white flex items-center border-b border-gray-200 shadow-md'>
+             <div className='bg-blue-600 h-full w-[74px] items-start mr-16'><img src={applogo} className='items-center justify-between size-[74px] object-contain'/></div>
              <h2 className='text-black text-xl font-extrabold mr-16'><span className='text-blue-600'>Pro</span><span className='text-gray-700'>Assign</span></h2>
               
               <div className='flex items-center justify-between space-x-8 ml-12 mr-8'>
